b34cd8d5113640f00c4a24b79f13e01f7a6ea43d
Your Name <you@example.com>
2018-02-14 11:12:50 -0500

    rename host

--- template/amp/dfp-creative.js
+++ template/amp/dfp-creative.js
@@ -1,14 +1,14 @@
 <script src = "https://cdn.jsdelivr.net/npm/prebid-universal-creative/dist/creative.js"></script>
 <script>
 var adId = "%%PATTERN:hb_adid%%";
-var host = "%%PATTERN:hb_cache_host%%";
+var cacheHost = "%%PATTERN:hb_cache_host%%";
 var cachePath = "%%PATTERN:hb_cache_path%%";
 var uuid = "%%PATTERN:hb_cache_id%%";
 var mediaType = "%%PATTERN:hb_format%%";
 var pubUrl = "%%PATTERN:url%%";
 
 try {    
-    pbjs.renderAd(document, adId, {host: host, cachePath: cachePath, uuid: uuid, mediaType: mediaType, pubUrl: pubUrl});
+    pbjs.renderAd(document, adId, {cacheHost: cacheHost, cachePath: cachePath, uuid: uuid, mediaType: mediaType, pubUrl: pubUrl});
 } catch(e) {
     console.log(e);
 }
