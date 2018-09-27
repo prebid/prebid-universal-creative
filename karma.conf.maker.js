0589ad35607b5b2171581b8b28f6faf016f8ed2f
Jason Snellbaker <jsnellbaker@appnexus.com>
2018-09-27 15:17:30 -0400

    add mocha reporter as base and keep coverage for testcoverage task

--- karma.conf.maker.js
+++ karma.conf.maker.js
@@ -28,6 +28,7 @@ function setReporters(karmaConf, codeCoverage, browserstack) {
     };
   }
   if (codeCoverage) {
+    karmaConf.reporters = ['progress', 'coverage'],
     karmaConf.coverageReporter = {
       reporters:[
         {
@@ -93,7 +94,11 @@ module.exports = function(codeCoverage, browserstack, watchMode) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['mocha'],
+    mochaReporter: {
+      showDiff: true,
+      output: 'minimal'
+    },
 
     // web server port
     port: 9876,
