079416e874549843b412cf40ebe83b720cd87d1d
Matt Lane <mlane@appnexus.com>
2019-02-20 14:14:24 -0800

    Add asset loader entry point

--- src/nativeTrackers.js
+++ src/nativeTrackers.js
@@ -1,6 +1,10 @@
+import { newNativeAssetManager } from './nativeAssetManager';
 import { newNativeTrackerManager } from './nativeTrackerManager';
 
 window.pbNativeTag = (window.pbNativeTag || {});
-const nativeTrackerManager = newNativeTrackerManager(window);
 
+const nativeTrackerManager = newNativeTrackerManager(window);
 window.pbNativeTag.startTrackers = nativeTrackerManager.startTrackers;
+
+const nativeAssetManager = newNativeAssetManager(window);
+window.pbNativeTag.loadAssets = nativeAssetManager.scanForPlaceholders;
