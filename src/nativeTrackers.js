32dcda900c55d7e564c071279f8fdd5c7e6551f1
Matt Lane <mlane@appnexus.com>
2019-02-21 17:21:41 -0800

    Revert nativeTrackers changes

--- src/nativeTrackers.js
+++ src/nativeTrackers.js
@@ -1,10 +1,6 @@
-import { newNativeAssetManager } from './nativeAssetManager';
 import { newNativeTrackerManager } from './nativeTrackerManager';
 
 window.pbNativeTag = (window.pbNativeTag || {});
-
 const nativeTrackerManager = newNativeTrackerManager(window);
-window.pbNativeTag.startTrackers = nativeTrackerManager.startTrackers;
 
-const nativeAssetManager = newNativeAssetManager(window);
-window.pbNativeTag.loadAssets = nativeAssetManager.scanForPlaceholders;
+window.pbNativeTag.startTrackers = nativeTrackerManager.startTrackers;
