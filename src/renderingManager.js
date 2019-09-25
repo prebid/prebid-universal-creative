b2960c6227048f29c7300ceff55a5bd05ca40e06
Isaac Dettman <idettman@rubiconproject.com>
2019-09-25 10:11:33 -0700

    updated to remove extra reference for test for bidid in winurl

--- src/renderingManager.js
+++ src/renderingManager.js
@@ -45,22 +45,20 @@ export function newRenderingManager(win, environment) {
       const targetingBidderIds = Object.keys(targetingData).filter(key => key.match(/bidderid_(\w)/));
       if (targetingBidderIds.length) {
         const winbidid = targetingBidderIds[0];
-        // test if BIDID exists in winurl
-        const matchWinUrl = targetingData.winurl.match(/=BIDID\b/);
-        // if BIDID doesn't exist log console warning
-        if (matchWinUrl) {
+        // test if BIDID exists in winurl, if BIDID doesn't exist log console warning
+        if (targetingData.winurl.match(/=BIDID\b/)) {
           const replacedUrl = targetingData.winurl.replace(/=BIDID\b/, `=${targetingData[winbidid]}`);
           try {
             triggerPixel(replacedUrl, function triggerPixelCallback(event) {
               if (event.type !== 'load') {
-                console.warn('failed to load pixel for winurl :%s', replacedUrl);
+                console.warn('failed to load pixel for winurl:', replacedUrl);
               }
             });
           } catch (e) {
-            console.warn('failed to get pixel for winurl: %s', replacedUrl);
+            console.warn('failed to get pixel for winurl:', replacedUrl);
           }
         } else {
-          console.warn('failed to find BIDID in winurl', targetingData.winurl);
+          console.warn('failed to find BIDID in winurl:', targetingData.winurl);
         }
       }
     }
