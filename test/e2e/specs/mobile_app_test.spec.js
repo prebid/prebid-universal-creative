e3b6ddb635fa4ea183bce3ce9a1b36566bdd722e
Neelanjan Sen <neelanjan.sen@gmail.com>
2019-11-14 11:40:31 +0530

    change https to http

--- test/e2e/specs/mobile_app_test.spec.js
+++ test/e2e/specs/mobile_app_test.spec.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('mobile app', function() {
   it('should load the creative', function() {
     browser
-      .url('https://test.localhost:9990/testpages/mobile-app_test.html')
+      .url('http://test.localhost:9990/testpages/mobile-app_test.html')
       .pause(4000);
 
     assert.equal(browser.isVisible('body > div > div > a > img'), true);
