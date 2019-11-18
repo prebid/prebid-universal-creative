b197608516bbe6cb2d06e46f7e5f8c5975b896d3
Jaimin Panchal <7393273+jaiminpanchal27@users.noreply.github.com>
2019-11-18 11:22:32 -0500

    Mobile native rendering (#83)
    * mobile native rendering
    
    * Adding missing files
    
    * check for undefined

--- src/nativeAssetManager.js
+++ src/nativeAssetManager.js
@@ -3,6 +3,8 @@
  * values in native creative templates.
  */
 
+import { sendRequest } from './utils';
+
 /*
  * Native asset->key mapping from Prebid.js/src/constants.json
  * https://github.com/prebid/Prebid.js/blob/8635c91942de9df4ec236672c39b19448545a812/src/constants.json#L67
@@ -27,9 +29,133 @@ const NATIVE_KEYS = {
   salePrice: 'hb_native_saleprice',
 };
 
+// Asset type mapping as per Native IAB spec 1.2
+// https://www.iab.com/wp-content/uploads/2017/04/OpenRTB-Native-Ads-Specification-Draft_1.2_2017-04.pdf#page=40
+const assetTypeMapping = {
+  'image': {
+    1: 'icon',
+    3: 'image'
+  },
+  'data': {
+    1: 'sponsoredBy',
+    2: 'body',
+    3: 'rating',
+    4: 'likes',
+    5: 'downloads',
+    6: 'price',
+    7: 'salePrice',
+    8: 'phone',
+    9: 'address',
+    10: 'body2',
+    11: 'displayUrl',
+    12: 'cta',
+  }
+}
+
+const DEFAULT_CACHE_HOST = 'prebid.adnxs.com';
+const DEFAULT_CACHE_PATH = '/pbc/v1/cache';
+
 export function newNativeAssetManager(win) {
   let callback;
   let errorCountEscapeHatch = 0;
+
+  function getCacheEndpoint(cacheHost, cachePath) {
+    let host = (typeof cacheHost === 'undefined' || cacheHost === "") ? DEFAULT_CACHE_HOST : cacheHost;
+    let path = (typeof cachePath === 'undefined' || cachePath === "") ? DEFAULT_CACHE_PATH : cachePath;
+  
+    return `https://${host}${path}`;
+  }
+  
+  function parseResponse(response) {
+    let bidObject;
+    try {
+      bidObject = JSON.parse(response);
+    } catch (error) {
+      console.log(`Error parsing response from cache host: ${error}`);
+    }
+    return bidObject;
+  }
+  
+  function transformToPrebidKeys(adMarkup) {
+    let assets = [];
+    let clicktrackers;
+    let assetsFromMarkup = adMarkup.assets;
+    assetsFromMarkup.forEach((asset) => {
+      if (asset.img) {
+        if (assetTypeMapping['image'][asset.img.type]) {
+          assets.push({
+            'key' : assetTypeMapping['image'][asset.img.type],
+            'value' : asset.img.url
+          })
+        } else {
+          console.log('ERROR: Invalid image type for image asset');
+        }
+      } else if (asset.data) {
+        if (assetTypeMapping['data'][asset.data.type]) {
+          assets.push({
+            'key' : assetTypeMapping['data'][asset.data.type],
+            'value' : asset.data.value
+          })
+        } else {
+          console.log('ERROR: Invalid data type for data asset');
+        }
+      } else if (asset.title) {
+        assets.push({
+          'key' : 'title',
+          'value' : asset.title.text
+        })
+      } 
+    })
+
+    if (adMarkup.link) {
+      if (adMarkup.link.clicktrackers) {
+        clicktrackers = adMarkup.link.clicktrackers;
+      }
+      assets.push({
+        'key' : 'clickUrl',
+        'value' : adMarkup.link.url
+      })
+    }
+
+    return {
+      assets,
+      clicktrackers,
+      'imptrackers' : adMarkup.imptrackers
+    }
+  }
+
+  function requestAssetsFromCache(tagData) {
+    let ajaxCallback = function(response) {
+      let bidResponse = parseResponse(response);
+      if (bidResponse && bidResponse.adm) {
+        let markup = parseResponse(bidResponse.adm);
+        if (markup && markup.assets) {
+          let data = transformToPrebidKeys(markup);
+          const body = win.document.body.innerHTML;
+          const newHtml = replace(body, data);
+          win.document.body.innerHTML = newHtml;
+
+          callback && callback({
+            clickTrackers: data.clicktrackers, 
+            impTrackers: data.imptrackers
+          });
+        } else {
+          // TODO Shall we just write the markup in the page
+        }
+      }
+    }
+    let uuid = tagData.uuid;
+    let adUrl = `${getCacheEndpoint(tagData.cacheHost, tagData.cachePath)}?uuid=${uuid}`;
+    sendRequest(adUrl, ajaxCallback);
+  }
+
+  function loadMobileAssets(tagData, cb) {
+    const placeholders = scanForPlaceholders();
+    if (placeholders.length > 0) {
+      callback = cb;
+      requestAssetsFromCache(tagData);
+    }
+  }
 
   /*
    * Entry point to search for placeholderes and set up postmessage roundtrip
@@ -53,7 +179,7 @@ export function newNativeAssetManager(win) {
 
     Object.keys(NATIVE_KEYS).forEach(key => {
       const placeholderKey = NATIVE_KEYS[key];
-      const placeholder = `${placeholderKey}:${adId}`;
+      const placeholder = (adId) ? `${placeholderKey}:${adId}` : `${placeholderKey}`;
       const placeholderIndex = win.document.body.innerHTML.indexOf(placeholder);
 
       if (~placeholderIndex) {
@@ -118,13 +244,15 @@ export function newNativeAssetManager(win) {
     let html = document;
 
     (assets || []).forEach(asset => {
-      html = html.replace(`${NATIVE_KEYS[asset.key]}:${adId}`, asset.value);
+      const searchString = (adId) ? `${NATIVE_KEYS[asset.key]}:${adId}` : `${NATIVE_KEYS[asset.key]}`
+      html = html.replace(searchString, asset.value);
     });
 
     return html;
   }
 
   return {
-    loadAssets
+    loadAssets,
+    loadMobileAssets
   };
 }
--- test/spec/nativeAssetManager_spec.js
+++ test/spec/nativeAssetManager_spec.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { merge } from 'lodash';
 import { newNativeAssetManager } from 'src/nativeAssetManager';
 import { mocks } from 'test/helpers/mocks';
+import * as utils from 'src/utils';
 
 const AD_ID = 'abc123';
 
@@ -80,4 +81,33 @@ describe('nativeTrackerManager', () => {
 
     expect(win.document.body.innerHTML).to.equal(html);
   });
+
+  it('replace mobile native placeholder with their values', function() {
+    win.document.body.innerHTML = `
+      <h1>hb_native_cta</h1>
+      <p>hb_native_body</p>
+      <a href="hb_native_linkurl">Click Here</a>
+    `;
+
+    let cb = sinon.spy();
+    let targetingData = {
+      uuid: '123'
+    }
+
+    sinon.stub(utils, 'sendRequest').callsFake(function(arg1, cb) {
+      let response = JSON.stringify({"id":"6572251357847878203","impid":"some-imp-id","price":10,"adm":"{\"assets\":[{\"id\":1,\"img\":{\"type\":3,\"url\":\"http://vcdn.adnxs.com/p/creative-image/f8/7f/0f/13/f87f0f13-230c-4f05-8087-db9216e393de.jpg\",\"w\":989,\"h\":742,\"ext\":{\"appnexus\":{\"prevent_crop\":0}}}},{\"title\":{\"text\":\"This is a Prebid Native Creative\"}},{\"id\":2,\"data\":{\"type\":1,\"value\":\"Prebid.org\"}},{\"id\":3,\"data\":{\"type\":2,\"value\":\"new value\"}}],\"link\":{\"url\":\"http://example.com\"},\"imptrackers\":[\"http://some-tracker.com\"],\"jstracker\":\"\\u003cscript type=\\\"text/javascript\\\" async=\\\"true\\\" src=\\\"http://cdn.adnxs.com/v/app/179/trk.js#app;vk=appnexus.com-omid;tv=app-native-23h;dom_id=%native_dom_id%;st=2;d=1x1;vc=iab;vid_ccr=1;tag_id=13232354;cb=http%3A%2F%2Fnym1-ib.adnxs.com%2Fvevent%3Fan_audit%3D0%26test%3D1%26e%3DwqT_3QLXB2zXAwAAAwDWAAUBCJqev-0FEN2mtMzg8dSSPxj_EQEQASo2CQAFAQgkQBEFCAwAJEAZEQkAIREJACkRCQAxEQmoMOLRpwY47UhA7UhIAlC8yb4uWJzxW2AAaM26dXjjrAWAAQGKAQNVU0SSAQEG8FKYAQGgAQGoAQGwAQC4AQLAAQPIAQLQAQDYAQDgAQHwAQCKAjt1ZignYScsIDI1Mjk4ODUsIDE1NzE4MDI5MDYpO3VmKCdyJywgOTc0OTQyMDQsIC4eAPQOAZICuQIhTEVEX1hnajgtTHdLRUx6SnZpNFlBQ0NjOFZzd0FEZ0FRQVJJN1VoUTR0R25CbGdBWVBfX19fOFBhQUJ3QVhnQmdBRUJpQUVCa0FFQm1BRUJvQUVCcUFFRHNBRUF1UUh6cldxa0FBQWtRTUVCODYxcXBBQUFKRURKQWJSM21tYW5MdWNfMlFFQUFBQUFBQUR3UC1BQkFQVUJBQUFBQUpnQ0FLQUNBTFVDQUFBQUFMMENBQUFBQU1BQ0FjZ0NBZEFDQWRnQ0FlQUNBT2dDQVBnQ0FJQURBWmdEQWFnRF9QaThDcm9EQ1U1WlRUSTZORFl6Tk9BRHBSU0lCQUNRQkFDWUJBSEJCQUFBQUEJgwh5UVEJCQEBGE5nRUFQRUUBCwkBUEQ0QkFDSUJab2uaAokBIVp3LWhMQTY9ASRuUEZiSUFRb0FEFThUa1FEb0pUbGxOTWpvME5qTTBRS1VVUxFoDFBBX1URDAxBQUFXHQwAWR0MAGEdDABjHQzweWVBQS7YAgDgAq2YSIADAYgDAJADAJgDFKADAaoDAMAD4KgByAMA2AMA4AMA6AMC-AMAgAQAkgQJL29wZW5ydGIymAQAqAQAsgQMCAAQABgAIAAwADgAuAQAwAQAyAQA0gQOOTMyNSNOWU0yOjQ2MzTaBAIIAeAEAPAEQcCQggUab3JnLnByZWJpZC5tb2JpbGUuYXBpMWRlbW-IBQGYBQCgBXE4QP8BqgUHc29tZS1pZMAFAMkFaRgU8D_SBQkJCQw8AADYBQHgBQHwBZn0IfoFBAGUKJAGAZgGALgGAMEGCSU48D_IBgDQBvUv2gYWChAAOgEAUBAAGADgBgzyBgIIAIAHAYgHAKAHQQ..%26s%3Da7b19f6eede870d487a7bec88354794855bf8161;ts=1571802906;cet=0;cecb=\\\"\\u003e\\u003c/script\\u003e\"}","adid":"97494204","adomain":["http://prebid.org"],"iurl":"http://nym1-ib.adnxs.com/cr?id=97494204","cid":"9325","crid":"97494204","cat":["IAB3-1"],"ext":{"appnexus":{"brand_id":555545,"auction_id":4550134868038456157,"bidder_id":2,"bid_ad_type":3}}});
+      cb(response);
+    });
+
+    const nativeAssetManager = newNativeAssetManager(win);
+    nativeAssetManager.loadMobileAssets(targetingData, cb);
+
+    expect(win.document.body.innerHTML).to.include('<p>new value</p>');
+    expect(win.document.body.innerHTML).to.include(`
+      <a href="http://example.com">Click Here</a>
+    `);
+    // cta was not a requested asset so this should stay as is
+    expect(win.document.body.innerHTML).to.include('<h1>hb_native_cta</h1>');
+    utils.sendRequest.restore();
+  })
 });
