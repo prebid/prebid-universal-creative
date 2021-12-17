91d1bdc28b09f6105ce507f0159b9c55ed945769
Filip Stamenkovic <ficadub@gmail.com>
2021-12-17 09:34:52 +0100

    fix tests

--- test/spec/nativeRenderManager_spec.js
+++ test/spec/nativeRenderManager_spec.js
@@ -1,4 +1,5 @@
 import { newNativeRenderManager } from 'src/nativeRenderManager';
+import * as nam  from 'src/nativeAssetManager';
 import { expect } from 'chai';
 import { mocks } from 'test/helpers/mocks';
 import { merge } from 'lodash';
@@ -22,6 +23,17 @@ describe('nativeRenderManager', function () {
   describe('load renderNativeAd', function () {
     let mockWin;
     let consoleWarn;
+    let assetManagerStub;
+
+    before(function() {
+      assetManagerStub = sinon.stub(nam, 'newNativeAssetManager').callsFake(() => {
+        return {
+          loadAssets: (adId, callback) => {
+            callback();
+          }
+        }
+      });
+    });
 
     let tagData = {
       pubUrl: 'http://example.com',
@@ -36,58 +48,63 @@ describe('nativeRenderManager', function () {
 
     afterEach(function () {
       consoleWarn.restore();
+      assetManagerStub.resetHistory();
     });
 
-    // it('should verify the postMessage for impression trackers was executed', function() {
-    //   mockWin.document.getElementsByClassName = () => [{
-    //     attributes: {
-    //       pbAdId: {
-    //         value: 'ad123'
-    //       }
-    //     },
-    //     addEventListener: (type, listener, capture) => {
-    //     },
-    //   }];
-    //   let nativeTracker = new newNativeRenderManager(mockWin);
-    //   nativeTracker.renderNativeAd(tagData);
+    after(function() {
+      assetManagerStub.restore();
+    });
 
-    //   // expect(mockWin.parent.postMessage.callCount).to.equal(1);
-    //   let postMessageTargetDomain = mockWin.parent.postMessage.args[0][1];
-    //   let postMessageContents = mockWin.parent.postMessage.args[0][0];
-    //   let rawPostMessage = JSON.parse(postMessageContents);
+    it('should verify the postMessage for impression trackers was executed', function() {
+      mockWin.document.getElementsByClassName = () => [{
+        attributes: {
+          pbAdId: {
+            value: 'ad123'
+          }
+        },
+        addEventListener: (type, listener, capture) => {
+        },
+      }];
+      let nativeTracker = new newNativeRenderManager(mockWin);
+      nativeTracker.renderNativeAd(tagData);
 
-    //   expect(rawPostMessage.message).to.exist.and.to.equal("Prebid Native");
-    //   expect(rawPostMessage.adId).to.exist.and.to.equal("ad123");
-    //   expect(rawPostMessage.action).to.not.exist;
-    //   expect(trimPort(postMessageTargetDomain)).to.equal(tagData.pubUrl);
-    // });
+      expect(mockWin.parent.postMessage.callCount).to.equal(1);
+      let postMessageTargetDomain = mockWin.parent.postMessage.args[0][1];
+      let postMessageContents = mockWin.parent.postMessage.args[0][0];
+      let rawPostMessage = JSON.parse(postMessageContents);
 
-    // it('should verify the postMessages for the impression and click trackers were executed', function() {
-    //   mockWin.document.getElementsByClassName = () => [{
-    //     attributes: {
-    //       pbAdId: {
-    //         value: 'ad123'
-    //       }
-    //     },
-    //     addEventListener: ((type, listener, capture) => {
-    //       listener({
-    //       })
-    //     })
-    //   }];
+      expect(rawPostMessage.message).to.exist.and.to.equal("Prebid Native");
+      expect(rawPostMessage.adId).to.exist.and.to.equal("ad123");
+      expect(rawPostMessage.action).to.not.exist;
+      expect(trimPort(postMessageTargetDomain)).to.equal(tagData.pubUrl);
+    });
 
-    //   let nativeTracker = new newNativeRenderManager(mockWin);
-    //   nativeTracker.renderNativeAd(tagData);
+    it('should verify the postMessages for the impression and click trackers were executed', function() {
+      mockWin.document.getElementsByClassName = () => [{
+        attributes: {
+          pbAdId: {
+            value: 'ad123'
+          }
+        },
+        addEventListener: ((type, listener, capture) => {
+          listener({
+          })
+        })
+      }];
 
-    //   expect(mockWin.parent.postMessage.callCount).to.equal(2);
+      let nativeTracker = new newNativeRenderManager(mockWin);
+      nativeTracker.renderNativeAd(tagData);
 
-    //   let postMessageTargetDomain = mockWin.parent.postMessage.args[0][1];
-    //   let postMessageContents = mockWin.parent.postMessage.args[0][0];
-    //   let rawPostMessage = JSON.parse(postMessageContents);
+      expect(mockWin.parent.postMessage.callCount).to.equal(2);
 
-    //   expect(rawPostMessage.message).to.exist.and.to.equal("Prebid Native");
-    //   expect(rawPostMessage.adId).to.exist.and.to.equal("ad123");
-    //   expect(rawPostMessage.action).to.exist.and.to.equal('click');
-    //   expect(trimPort(postMessageTargetDomain)).to.equal(tagData.pubUrl);
-    // });
+      let postMessageTargetDomain = mockWin.parent.postMessage.args[0][1];
+      let postMessageContents = mockWin.parent.postMessage.args[1][0];
+      let rawPostMessage = JSON.parse(postMessageContents);
+
+      expect(rawPostMessage.message).to.exist.and.to.equal("Prebid Native");
+      expect(rawPostMessage.adId).to.exist.and.to.equal("ad123");
+      expect(rawPostMessage.action).to.exist.and.to.equal('click');
+      expect(trimPort(postMessageTargetDomain)).to.equal(tagData.pubUrl);
+    });
   });
 });
