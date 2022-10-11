bd66f43fcaa98adae980e536dc93cd9148d2f2a8
Demetrio Girardi <dgirardi@prebid.org>
2022-10-11 06:55:10 -0700

    Increase expect timeout

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 10000
+    timeout: 15000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
