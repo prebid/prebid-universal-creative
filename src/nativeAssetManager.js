5e8a24fe29d287900e78cfb1c7123b0b9efc8d81
Demetrio Girardi <dgirardi@prebid.org>
2023-08-28 08:15:41 -0700

    Improve resize logic for native (#193)

--- src/nativeAssetManager.js
+++ src/nativeAssetManager.js
@@ -344,7 +344,7 @@ export function newNativeAssetManager(win, nativeTag) {
       } else if ((data.hasOwnProperty('adTemplate') && data.adTemplate)||(hasPbNativeData() && win.pbNativeData.hasOwnProperty('adTemplate'))) {
         const template =  (hasPbNativeData() && win.pbNativeData.hasOwnProperty('adTemplate') && win.pbNativeData.adTemplate) || data.adTemplate;
         const newHtml = replace(template, data);
-        
+
         renderAd(newHtml, data);
       } else {
         const newHtml = replace(body, data);
@@ -366,7 +366,7 @@ export function newNativeAssetManager(win, nativeTag) {
         if (!currentParentWindow.frames || !currentParentWindow.frames.length) return null;
         for (let idx = 0; idx < currentParentWindow.frames.length; idx++)
             if (currentParentWindow.frames[idx] === currentWindow) {
-              if (!currentParentWindow.document) return null; 
+              if (!currentParentWindow.document) return null;
                 for (let frameElement of currentParentWindow.document.getElementsByTagName('iframe')) {
                     if (!frameElement.contentWindow) return null;
                     if (frameElement.contentWindow === currentWindow) {
@@ -385,7 +385,7 @@ export function newNativeAssetManager(win, nativeTag) {
     if (!isSafeFrame(window)) {
       let iframeContainer = getCurrentFrameContainer(win);
       if (iframeContainer && iframeContainer.children && iframeContainer.children[0]) {
-        const iframe = iframeContainer.children[0]; 
+        const iframe = iframeContainer.children[0];
         if (iframe.width === '1' && iframe.height === '1') {
           let width =  iframeContainer.getBoundingClientRect().width;
           win.document.body.style.width = `${width}px`;
@@ -400,7 +400,8 @@ export function newNativeAssetManager(win, nativeTag) {
     callback && callback();
     win.removeEventListener('message', replaceAssets);
     stopListening();
-    requestHeightResize(bid.adId, (document.body.clientHeight || document.body.offsetHeight), document.body.clientWidth);
+    const resize = () => requestHeightResize(bid.adId, (document.body.clientHeight || document.body.offsetHeight), document.body.clientWidth);
+    document.readyState === 'complete' ? resize() : window.onload = resize;
 
     if (typeof window.postRenderAd === 'function') {
       window.postRenderAd(bid);
@@ -441,7 +442,7 @@ export function newNativeAssetManager(win, nativeTag) {
     if (ortb.link) {
       html = html.replaceAll("##hb_native_linkurl##", ortb.link.url);
     }
-  
+
     return html;
   }
 
