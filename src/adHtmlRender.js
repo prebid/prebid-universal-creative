3d62301dd24e1e5236900d960250a18d9f8d39c7
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2024-12-03 09:09:56 +0200

    Fix mobile-app rendering when admarkup contains lowercase DOCTYPE tag

--- src/adHtmlRender.js
+++ src/adHtmlRender.js
@@ -1,7 +1,7 @@
 export function writeAdHtml(markup, insertHTML = document.body.insertAdjacentHTML.bind(document.body)) {
     // remove <?xml> and <!doctype> tags
     // https://github.com/prebid/prebid-universal-creative/issues/134
-    markup = markup.replace(/\<(\?xml|(\!DOCTYPE[^\>\[]+(\[[^\]]+)?))+[^>]+\>/g, '');
+    markup = markup.replace(/\<(\?xml|(\!DOCTYPE[^\>\[]+(\[[^\]]+)?))+[^>]+\>/gi, '');
 
     try {
         insertHTML('beforeend', markup);
