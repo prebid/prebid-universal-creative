bc3010be200c4080060229ada5a49e5ffab61db3
Ricky Blaha <ricky.blaha@yahooinc.com>
2024-12-19 02:27:45 -0500

    Reduce mobile app ad discprepancy: render the markup immediately only if there is a burl present. If no burl, render the markup only after mraid viewable happens.
    * Added console logs to observe this behavior that stick around in `window.pucLogs` so they can be viewed once a web view is inspected after render has already taken place.
    * Added simple MRAID 2 and 3 for use in the unit tests for this change.

--- test/spec/mobileAndAmpRender_spec.js
+++ test/spec/mobileAndAmpRender_spec.js
@@ -4,8 +4,9 @@ import * as utils from 'src/utils';
 import { expect } from 'chai';
 import { mocks } from 'test/helpers/mocks';
 import { merge } from 'lodash';
-import {writeAdHtml} from 'src/postscribeRender';
+import { writeAdHtml } from 'src/postscribeRender';
 
+import { createMraid2Mock, createMraid3Mock } from 'test/helpers/mraid-mocks';
 
 function renderingMocks() {
   return {
@@ -148,12 +149,270 @@ describe("renderingManager", function () {
         height: 250,
         crid: 123,
         adm: "ad-markup",
+        // Immediate write of html happens only if burl is present.
+        // Without burl ad markup write waits until MRAID-visible.
+        burl: 'https://127.0.0.1/burl'
       };
       requests[0].respond(200, {}, JSON.stringify(response));
       expect(writeHtmlSpy.callCount).to.equal(1);
       expect(sendRequestSpy.args[0][0]).to.equal(
         "https://prebid.adnxs.com/pbc/v1/cache?uuid=123"
       );
+    });
+
+    describe("bids without burl", () => {
+      const ucTagData = {
+        cacheHost: "example.com",
+        cachePath: "/path",
+        uuid: "123",
+        size: "300x250",
+        env: "mobile-app",
+        hb_cache_region: "us-east-1"
+      };
+
+      const response = {
+        width: 300,
+        height: 250,
+        crid: 123,
+        adm: "ad-markup",
+        // No billing url "burl" property.
+      };
+
+      let mraidMock;
+      let loadScriptStub;
+      beforeEach(() => {
+        loadScriptStub = sinon.stub(utils, "loadScript");
+      });
+      afterEach(() => {
+        if (loadScriptStub.restore) {
+          loadScriptStub.restore();
+        }
+        if (mraidMock) {
+          mraidMock.cleanup();
+        }
+      });
+
+      describe('MRAID 2 - not initially ready, not visible until later', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid2Mock({isReady: false, isViewable: false});
+
+          // Not ready at time of script load callback.
+          loadMraidScriptCallback();
+
+          // Now ready, but not yet visible.
+          mraidMock.setReady(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Now make it visible.
+          mraidMock.setViewable(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 2 - not initially ready, visible when ready', () => {
+        it('should writeHtml only after MRAID visible happens', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid2Mock({isReady: false, isViewable: false});
+
+          // Not ready at time of script load callback.
+          loadMraidScriptCallback();
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          mraidMock.setViewable(true);
+
+          // Ready and visible.
+          mraidMock.setReady(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 2 - initially ready, not visible until later', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid2Mock({isReady: false, isViewable: false});
+
+          mraidMock.setReady(true);
+
+          // Ready initially, but not yet visible.
+          loadMraidScriptCallback();
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Now make it visible.
+          mraidMock.setViewable(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 2 - initially ready and visible', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid2Mock({isReady: false, isViewable: false});
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          mraidMock.setViewable(true);
+          mraidMock.setReady(true);
+
+          // Ready and visible at the time of script load callback.
+          loadMraidScriptCallback();
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 3 - not initially ready, not visible until later', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid3Mock({isReady: false, isViewable: false});
+
+          // Not ready at time of script load callback.
+          loadMraidScriptCallback();
+
+          // Now ready, but not yet visible.
+          mraidMock.setReady(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Now make it visible.
+          mraidMock.setViewable(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 3 - not initially ready, visible when ready', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid3Mock({isReady: false, isViewable: false});
+
+          // Not ready at time of script load callback.
+          loadMraidScriptCallback();
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          mraidMock.setViewable(true);
+
+          // Ready and visible.
+          mraidMock.setReady(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 3 - initially ready, not visible', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid3Mock({isReady: false, isViewable: false});
+
+          mraidMock.setReady(true);
+
+          // Ready initially, but not yet visible.
+          loadMraidScriptCallback();
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Now make it visible.
+          mraidMock.setViewable(true);
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
+
+      describe('MRAID 3 - initially ready and visible', () => {
+        it('should writeHtml only after MRAID visible', function () {
+          renderAmpOrMobileAd(ucTagData, true);
+
+          requests[0].respond(200, {}, JSON.stringify(response));
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          // Simulate mraid.js being loaded.
+          expect(loadScriptStub.callCount).to.equal(1);
+
+          let loadMraidScriptCallback = loadScriptStub.getCall(0).args[2];
+
+          mraidMock = createMraid3Mock({isReady: false, isViewable: false});
+
+          expect(writeHtmlSpy.callCount).to.equal(0);
+
+          mraidMock.setViewable(true);
+          mraidMock.setReady(true);
+
+          // Ready and visible at the time of script load callback.
+          loadMraidScriptCallback();
+
+          expect(writeHtmlSpy.callCount).to.equal(1);
+        });
+      });
     });
 
   //   it('should catch errors from creative', function (done) {
